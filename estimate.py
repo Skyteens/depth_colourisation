bc3685e36f8227d49c3dd5b1dad96c20edf502f6
Skyteens <32707682+Skyteens@users.noreply.github.com>
2022-02-23 23:03:20 +0000

    Update images

--- estimate.py
+++ estimate.py
@@ -56,10 +56,3 @@ def estimate(model,num=0,imgPath="./test_imgs"):
 
     return fake_resize,gray
    
-    
-# if __name__ == "__main__":
-#     depthModel = colorizer_init()
-#     pred,gray  = estimate(depthModel,5)
-#     new= np.concatenate([gray, pred], 1)
-#     plt.imshow(new)
-#     plt.show()
